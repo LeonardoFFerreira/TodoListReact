26a915daf09b29a84894518b3449b690eb7ce9a0
LeonardoFFerreira <devleofreitas@gmail.com>
2021-10-31 18:14:00 -0300

    Criando function para add a nova tarefa e atualizar a lista antiga

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,22 @@ const App = () => {
   const [list, setList] = useState<Item[]>([
     { id: 1, tarefa: 'teste', executada: false }
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      tarefa: taskName,
+      executada: false
+    });
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>LISTA DE TAREFAS</C.Header>
+        <AddArea onEnter={handleAddTask}></AddArea>
         {list.map((item, index) => (
           <InputItem key={index} item={item} />
         ))}
--- src/components/add-area/index.tsx
+++ src/components/add-area/index.tsx
@@ -1,7 +1,19 @@
+import { useState, KeyboardEvent } from 'react';
 import * as C from './styles';
 
 export const AddArea = () => {
+
+    const [inputText, setInputText] = useState('');
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if (e.code === 'Enter' && inputText !== '') {
+
+        }
+    }
+
     return (
-        <C.Container></C.Container>
+        <C.Container>
+            <div className="image">➕</div>
+            <input type="text" placeholder="Adicione uma tarefa" value={inputText} onChange={e => setInputText(e.target.value)} onKeyUp={handleKeyUp} />
+        </C.Container>
     );
 }
